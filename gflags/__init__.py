5c409d6ec716cf3409f5cb14b09f163ffa1fc91e
John Asmuth <jasmuth@google.com>
2013-11-08 17:05:33 -0500

    git-ization of oauth2client.

---
+++ gflags/__init__.py
@@ -0,0 +1,7 @@
+"""Remap google3.pyglib.flags to gflags.
+
+The open source version of google3.pyglib.flags is gflags. Map it here under the
+expected name of gflags so our syncing of apiclient can remain pristine.
+"""
+
+from google3.pyglib.flags import *
