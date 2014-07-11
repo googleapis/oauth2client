df59cadb0398bae43b762d45dfa229127ffa734d
Orest Bolohan <orest@google.com>
2014-07-11 13:09:18 -0700

    Replace the 'default credentials' concept with the 'application default credentials' concept.

--- samples/call_compute_service.py
+++ samples/call_compute_service.py
@@ -1,13 +1,14 @@
-# To be used to test GoogleCredential.GetDefaultCredential()
+# To be used to test GoogleCredentials.get_application_default()
 # from local machine and GCE.
 
 from googleapiclient.discovery import build
 from oauth2client.client import GoogleCredentials
 
-PROJECT = "bamboo-machine-422"  # Provide your own GCE project here
-ZONE = "us-central1-a"          # Put here a zone which has some VMs
+PROJECT = 'bamboo-machine-422'  # Provide your own GCE project here
+ZONE = 'us-central1-a'          # Put here a zone which has some VMs
 
-service = build("compute", "v1", credentials=GoogleCredentials.get_default())
+credentials = GoogleCredentials.get_application_default()
+service = build('compute', 'v1', credentials=credentials)
 
 request = service.instances().list(project=PROJECT, zone=ZONE)
 response = request.execute()
