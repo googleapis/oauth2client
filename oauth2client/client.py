9eec4a513d96526f6c4b73e65b9c65deebd29de8
Pat Ferate <pferate+github@gmail.com>
2014-07-16 15:00:57 -0700

    Assume it is a str first, then convert to str if it fails.

--- oauth2client/client.py
+++ oauth2client/client.py
@@ -239,11 +239,11 @@ class Credentials(object):
       to_json().
     """
     try:
-        # s: bytes -> str
-        data = simplejson.loads(bytes.decode(s))
-    except TypeError:
         # s: already str
         data = simplejson.loads(s)
+    except TypeError:
+        # s: bytes -> str
+        data = simplejson.loads(bytes.decode(s))
     # Find and call the right classmethod from_json() to restore the object.
     module = data['_module']
     try:
@@ -579,11 +579,11 @@ class OAuth2Credentials(Credentials):
       An instance of a Credentials subclass.
     """
     try:
-        # s: bytes -> str
-        data = simplejson.loads(bytes.decode(s))
-    except TypeError:
         # s: already str
         data = simplejson.loads(s)
+    except TypeError:
+        # s: bytes -> str
+        data = simplejson.loads(bytes.decode(s))
     if 'token_expiry' in data and not isinstance(data['token_expiry'],
         datetime.datetime):
       try:
@@ -870,11 +870,11 @@ class AccessTokenCredentials(OAuth2Credentials):
   @classmethod
   def from_json(cls, s):
     try:
-        # s: bytes -> str
-        data = simplejson.loads(bytes.decode(s))
-    except TypeError:
         # s: already str
         data = simplejson.loads(s)
+    except TypeError:
+        # s: bytes -> str
+        data = simplejson.loads(bytes.decode(s))
     retval = AccessTokenCredentials(
         data['access_token'],
         data['user_agent'])
