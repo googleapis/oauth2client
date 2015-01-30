933e1d0073d676d6a80d39616405b19fc6f7ccbd
Aaron <aaronwinter@users.noreply.github.com>
2015-01-30 13:47:30 -0500

    Modify test to check for equality
    Use '==' or '!=' instead of 'is' or 'is not'.

--- oauth2client/client.py
+++ oauth2client/client.py
@@ -974,7 +974,7 @@ def _get_environment(urlopen=None):
     SETTINGS.env_name = 'GAE_PRODUCTION'
   elif server_software.startswith('Development/'):
     SETTINGS.env_name = 'GAE_LOCAL'
-  elif NO_GCE_CHECK is not 'True' and _detect_gce_environment(urlopen=urlopen):
+  elif NO_GCE_CHECK != 'True' and _detect_gce_environment(urlopen=urlopen):
     SETTINGS.env_name = 'GCE_PRODUCTION'
 
   return SETTINGS.env_name
