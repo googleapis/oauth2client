d1dc2b10b7cfc6aff6433ca32e257d29965dedee
Danny Hermes <daniel.j.hermes@gmail.com>
2015-08-05 17:10:32 -0700

    Adding favicon to match logo.

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,11 @@ if os.environ.get('READTHEDOCS', '') != 'True':
   html_theme = 'sphinx_rtd_theme'
   html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/favicon.ico'
+
 html_static_path = ['_static']
 html_logo = '_static/google_logo.png'
 htmlhelp_basename = 'oauth2clientdoc'
