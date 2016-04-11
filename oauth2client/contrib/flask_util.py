dd33fcccd22549f059e43ea649a86b7e9b4323d3
Bill Prin <waprin@google.com>
2016-04-11 12:20:03 -0700

    Pop Oauth flow rather then get

--- oauth2client/contrib/flask_util.py
+++ oauth2client/contrib/flask_util.py
@@ -199,7 +199,7 @@ _CSRF_KEY = 'google_oauth2_csrf_token'
 def _get_flow_for_token(csrf_token):
     """Retrieves the flow instance associated with a given CSRF token from
     the Flask session."""
-    flow_pickle = session.get(
+    flow_pickle = session.pop(
         _FLOW_KEY.format(csrf_token), None)
 
     if flow_pickle is None:
